b4899d2cb1517cfb979fa3d616a100e264f322da
CelicaRayna <celica.raynna@gmail.com>
2022-08-24 08:45:01 -0300

    Resolvido o exercicio do dia

---
+++ Fundamentos/bloco4-JavaScript/dia4-objetos-e-funcoes/exercicio1.js
@@ -0,0 +1,22 @@
+let diasDaSemana = {
+   1 : 'domingo',
+   2 : 'segunda',
+   3 : 'terça',
+   4 : 'quarta',
+   5 : 'quinta',
+   6 : 'sexta',
+   7 : 'sábado',
+    };
+    
+    // console.log(diasDaSemana.1); // SyntaxError: Unexpected number
+     console.log(diasDaSemana['1']); // domingo
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem '   + player.age)
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes')
