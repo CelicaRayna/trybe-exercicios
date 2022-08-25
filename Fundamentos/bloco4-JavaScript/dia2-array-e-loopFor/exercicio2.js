7005b960f364e103b755e67dda5f8e4ee20e87c8
CelicaRayna <celica.raynna@gmail.com>
2022-08-25 15:42:37 -0300

    Adicionado diretorio pra armazenar bloco 5

--- Fundamentos/bloco4-JavaScript/dia2-array-e-loopFor/exercicio2.js
+++ Fundamentos/bloco4-JavaScript/dia2-array-e-loopFor/exercicio2.js
@@ -1,5 +1,5 @@
 // Divisao
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma= 0;
 
 
@@ -8,3 +8,29 @@ soma += numbers[index]
 }
 let media = soma / numbers.length
 console.log(media)
+
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (index = 0; index < groceryList.length; index +=1) {
+// console.log(groceryList[index])
+// }
+for (let value of groceryList)
+console.log(value)
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let value of names);
+console.log(value);
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smallestNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+  }
+}
+
+console.log(smallestNumber);
+
--- Fundamentos/bloco4-JavaScript/dia3-logica-de-programacao-e-algoritmos/script.js
+++ Fundamentos/bloco4-JavaScript/dia3-logica-de-programacao-e-algoritmos/script.js
@@ -5,3 +5,25 @@
 }
 console.log(fatorial);
 
+let word = 'tryber';
+let reverseWord = '';
+
+reverseWord = word.split('').reverse().join('');
+
+console.log(reverseWord);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
