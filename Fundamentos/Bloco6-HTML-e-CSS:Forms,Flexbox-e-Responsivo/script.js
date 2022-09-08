3c0f97ec21a87cfd7cbfb74ea636c509d8215d9e
CelicaRayna <celica.raynna@gmail.com>
2022-09-08 10:32:32 -0300

    Adicionado click do botao de enviar e resetar

--- Fundamentos/Bloco6-HTML-e-CSS:Forms,Flexbox-e-Responsivo/script.js
+++ Fundamentos/Bloco6-HTML-e-CSS:Forms,Flexbox-e-Responsivo/script.js
@@ -3,8 +3,21 @@ function handleSubmit (event) {
     event.prevenDefault()
 }
 
+function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  }
+
 window.onload = function () {
-    let buttonSubmit= document.querySelector('button');
+    const clearBtn = document.querySelector('#clear-btn');
+  clearBtn.addEventListener('click', clearFields);
+    let buttonSubmit= document.querySelector('#submit-btn');
     buttonSubmit.addEventListener('click, handleSubmit')
 }
     
